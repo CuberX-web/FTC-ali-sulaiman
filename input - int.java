db276ce5280a2b31c101aa9de083c5786be9d915
Ramalishus <ramahmed5913@gmail.com>
2024-08-11 15:18:32 -0500

    Create input - int.java

---
+++ input - int.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        
+        System.out.print("Enter a number");
+        Scanner kb = new Scanner(System.in);
+        int n= kb.nextInt();
+        System.out.print(n);
+    }
+}
