bb3d02208e66354c96c20a38a89982a291e92955
CuberX-web <alibosso2010@gmail.com>
2024-08-29 17:26:36 -0500

    Create LFCintro.java

---
+++ LFCintro.java
@@ -0,0 +1,16 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+      //calling the function
+      returning(); 
+    }
+    public static void returning(){
+        //making variables
+         int a = 2;
+         int b = 3;
+         
+       System.out.println(a + b);
+     
+        
+    }
+}
